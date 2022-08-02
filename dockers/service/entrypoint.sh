df377f55ea32a0f4a33d999a3e93e3570d1210e9
Anton Tokarev <anton.tokarev@softeq.com>
2022-08-02 13:56:05 +0300

    migrations only in service container, crontab container wait service container flask start to serve http

--- dockers/service/entrypoint.sh
+++ dockers/service/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 PATH=$PATH:/usr/local/bin
 
 DB_HOST=$(echo ${DATABASE_URL} | sed -E -n 's/.*\@(.*)\:\b[0-9]{4,5}/\1/p')
@@ -7,6 +7,13 @@ until pg_isready -h "${DB_HOST}"; do
   echo "wait untill ${DB_HOST} is up.."
   sleep 1
 done
-flask db upgrade
+
+if [[ "${GITHUB_TOKEN}" = "" ]]; then
+  flask db upgrade
+else
+  until curl http://service:5000 -s -f -o /dev/null; do
+      echo "wait until service is up..."
+  done
+fi
 
 exec "$@"
